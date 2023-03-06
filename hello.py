d5500d0988e0f4784b867f9e9e0eb036b08a975c
Oluwatobi-hubb <oluwatobiwalex@gmail.com>
2023-03-06 22:10:27 +0000

    my replit commit

---
+++ hello.py
@@ -0,0 +1,7 @@
+print("""Tobi
+        March 6, 2023
+        I am signing up on Replit's 100 days of python challenge!
+        I will make sure to spend some time everyday coding along, for a minimum of ten minutes a day.
+        I'll be using Rrplit's, an amazing online IDE so i can do this on my phone wherever i happen to be. No excuses for not coding from tge middle of a field.!""")
+print("I am feeling exited, focused and determined")
+print("you can follow my progress at https://github.com/Oluwatobi-hub/Replit")
